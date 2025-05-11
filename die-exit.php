1068c53c55a00b6abecb2b0581f7cc7237b07fc3
SAURAV YADAV <91652627+IamSaurav07@users.noreply.github.com>
2025-05-11 09:14:23 +0530

    Add files via upload

---
+++ die-exit.php
@@ -0,0 +1,22 @@
+<?php
+    $a = 10;
+    $b = 20;
+    echo $a - $b;
+    echo "<br>";
+
+    echo $a + $b;
+    echo "<br>"; 
+    //break is only used in loops i.e. for,while
+    //die();          //execution stops after here
+    //exit;                 it's also    
+    exit("we can pass any statment also which will be executed after above statement can do for die() also" );
+    echo $a * $b;
+    echo "<br>";
+
+    echo $a / $b;
+    echo "<br>";
+
+    function test()   {
+        
+    }
+?>
