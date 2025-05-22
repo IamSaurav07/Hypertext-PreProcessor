34464a72a3376076ad4032915e32b5b8a6e2b5db
SAURAV YADAV <91652627+IamSaurav07@users.noreply.github.com>
2025-05-22 15:16:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    echo 30/10;
+    echo "<br/> First program in PHP<br/>";
+
+    $name = 'Saurav';
+    $age = '22';
+    $sum = 2*2;
+    $a = $b = 30;
+
+    echo $name;
+    echo '<br>';
+
+    echo $age;
+    echo '<br>';
+
+    echo $sum;
+    echo '<br>'
+?>
