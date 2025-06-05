4bed812f0f7dfb3277e774b7a768e56a5f542a46
SAURAV YADAV <91652627+IamSaurav07@users.noreply.github.com>
2025-06-05 17:34:53 +0530

    Add files via upload

---
+++ PhpWithHTML.php
@@ -0,0 +1,24 @@
+<?php
+$h3_color = "violet";
+    echo "<h1 style =  'color: green'>PHP with HTML</h1>";
+    echo "<h2 style =  'color: blue'>PHP with HTML</h2>";
+?>
+
+<?php
+    $name = "saurav";
+    echo "<h2 style = 'color : red'> My name is " . $name . "</h2>";
+?>
+
+
+<h1 style="color: tan;">
+    My real name is <?php 
+                $name1 = "Anan"; 
+                echo $name1 
+            ?>
+</h1>
+
+
+
+<h3 style="color: <?php echo $h3_color?>"> <?php echo "this is h2 tag"; ?></h3>
+<h3 style="color: <?php echo $h3_color?>"> <?php echo "this is h2 tag"; ?></h3>
+<h3 style="color: <?php echo $h3_color?>"> <?php echo "this is h2 tag"; ?></h3>
